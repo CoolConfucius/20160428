737f503c9a1a6a1ae2a899ddcd7e75e36fef698d
Benjamin Huang <coolconfucius@yahoo.com>
2016-05-02 12:54:28 -0700

    jsview

--- view/App.view.js
+++ view/App.view.js
@@ -1,3 +1,42 @@
-sap.ui.jsview("")
+sap.ui.jsview("sap.ui.demo.wt.controller.App", {
 
-sap.ui.demo.wt.controller.App
+  getControllerName: function() {
+    return "sap.ui.demo.wt.view.App"
+  },
+
+  createContent: function(oController) {
+    var homeBtn = new sap.m.Button(this.createId(homebutton), {
+      icon: "sap-icon://home",
+      tooltip: "Home",
+      press: function() {
+        window.history.go(-1); 
+      }
+    });
+
+    var headerLabel = sap.m.Label(this.createId("demoLabel"), {
+      text: "Demo"
+    }); 
+
+    var appHeader = new sap.m.Bar(this.createId("demoheader"), {
+      contentLeft: [homeBtn], 
+      contentMiddle: [headerLabel]
+    })
+
+    var labelmonth = new sap.m.label({
+      text: "Month-test", 
+      for: "moid"
+    })
+    var SubHeader = new sap.m.Bar(this.createId("subheader"), {
+      contentLeft: [labelmonth]
+    }); 
+    var verticalLayout = new sap.ui.layout.VerticalLayout({width: "94%"}); 
+
+    var page = new sap.m.Page(this.createId("demopage"),{
+      customHeader: appHeader, 
+      content: [SubHeader, verticalLayout]
+    })
+    return page; 
+  }
+
+})
+
