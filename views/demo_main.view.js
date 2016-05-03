8ede56706c2540654562d56d2f88cf86a1dcfcdf
Benjamin Huang <coolconfucius@yahoo.com>
2016-05-03 12:48:20 -0700

    1248

--- views/demo_main.view.js
+++ views/demo_main.view.js
@@ -13,8 +13,37 @@ sap.ui.jsview("demo.views.demo_main", {
 
     // })
 
-    var oPage = new sap.m.Page({
+    var headerLabel = new sap.m.Label(this.createId("demo-label"), {
+      text: "Demo"
+    }); 
 
+    var appHeader = new sap.m.Bar(this.createId("demo-headerBar"), {
+      contentMiddle: [headerLabel]
+    })
+
+    var HorizontalLayout = new sap.ui.layout.HorizontalLayout({
+      content: [
+        new sap.m.Panel({
+          content: [
+            new sap.ui.layout.Grid({
+              content: [
+                new sap.m.Label({
+                  text: "Account ID"
+                })
+              ]
+            })
+          ]
+        })
+      ]
+    })
+
+    var Subpanel = sap.m.Panel({
+      content: [HorizontalLayout]
+    })
+
+    var oPage = new sap.m.Page({
+      customHeader: appHeader, 
+      content: [Subpanel]
     })
 
     return oPage; 
