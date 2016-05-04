05f0c19d7bd9f52a0c2a551bddead5c083ad19d4
Benjamin Huang <coolconfucius@yahoo.com>
2016-05-04 16:05:18 -0700

    405

--- views/demo_main.controller.js
+++ views/demo_main.controller.js
@@ -24,20 +24,21 @@ sap.ui.controller("demo.views.demo_main", {
     var that=this;
     console.log("accCount");
 
-    $.ajax({
-      url: '/sap/ocm/account_settings/ui/services/as.xsodata/account/',
-      type: "GET",
-      cache: false, 
-      headers: {"X-Csrf-token" : sessionStorage.getItem("CSRF-Token")},
-      dataType: "json",
-      success: function(data) {
-        that.numberOfAccounts = data.d.results.length; 
-        that.acctitle.setText("Accounts ("+that.numberOfAccounts+")");
-      },
-      error: function(XMLHttpRequest, textStatus, errorThrown) {
-        sap.m.MessageToast.show("Error: "+XMLHttpRequest.responseText);
-      }
-    });
+    // $.ajax({
+    //   url: '/sap/ocm/account_settings/ui/services/as.xsodata/account/',
+    //   type: "GET",
+    //   cache: false, 
+    //   headers: {"X-Csrf-token" : sessionStorage.getItem("CSRF-Token")},
+    //   dataType: "json",
+    //   success: function(data) {
+    //     that.numberOfAccounts = data.d.results.length; 
+    //     that.acctitle.setText("Accounts ("+that.numberOfAccounts+")");
+    //   },
+    //   error: function(XMLHttpRequest, textStatus, errorThrown) {
+    //     sap.m.MessageToast.show("Error: "+XMLHttpRequest.responseText);
+    //   }
+    // });
+    that.acctitle.setText("Accounts (0)");
   },
 
   accountsarray:[],
