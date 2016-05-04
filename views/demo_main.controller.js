bea5790644298374b28f2d86b7e373095b04ab37
Benjamin Huang <coolconfucius@yahoo.com>
2016-05-04 16:04:05 -0700

    404

--- views/demo_main.controller.js
+++ views/demo_main.controller.js
@@ -24,9 +24,20 @@ sap.ui.controller("demo.views.demo_main", {
     var that=this;
     console.log("accCount");
 
-    // $.ajax({
-    //   url: '/sap/ocm/account...'
-    // })
+    $.ajax({
+      url: '/sap/ocm/account_settings/ui/services/as.xsodata/account/',
+      type: "GET",
+      cache: false, 
+      headers: {"X-Csrf-token" : sessionStorage.getItem("CSRF-Token")},
+      dataType: "json",
+      success: function(data) {
+        that.numberOfAccounts = data.d.results.length; 
+        that.acctitle.setText("Accounts ("+that.numberOfAccounts+")");
+      },
+      error: function(XMLHttpRequest, textStatus, errorThrown) {
+        sap.m.MessageToast.show("Error: "+XMLHttpRequest.responseText);
+      }
+    });
   },
 
   accountsarray:[],
